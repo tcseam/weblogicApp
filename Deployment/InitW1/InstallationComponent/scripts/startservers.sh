fe253e6f85dd03369adb09ac06f9a6714e91e11a
663479 <663479@01HW400162.India.TCS.com>
2016-09-23 13:13:57 +0530

    Commit 23/09/2016 13:13:57

--- Deployment/InitW1/InstallationComponent/scripts/startservers.sh
+++ Deployment/InitW1/InstallationComponent/scripts/startservers.sh
@@ -8,9 +8,8 @@ do
 ServerState=$(java weblogic.Admin -url 127.0.0.1:7001 -username wls -password weblogic@2016 GET -pretty -type ServerRuntime -property State |grep -i State |awk '{print $2}')
 if [ "$ServerState" == "RUNNING" ] ; then
 status=1
-sed -i 's/\r//' /home/DSEnv.sh
-bash ./home/DSEnv.sh
+sed -i 's/\r//' ./home/DSEnv.sh
+bash ./DSEnv.sh
 java weblogic.Deployer -adminurl t3://127.0.0.1:7001 -username wls -password weblogic@2016 -deploy -name InstallationComponent -source /home/InstallationComponent.ear
-#java weblogic.Deployer -adminurl t3://127.0.0.1:7001 -username wls -password weblogic@2016 -deploy -name InstallationComponent -source /home/InstallationComponent.ear
 fi
 done
